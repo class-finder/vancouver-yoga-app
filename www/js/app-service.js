e19fe5347459fa3b9b8060bda1c0153e0e8c00aa
Toby Sullivan <tobyjsullivan@gmail.com>
2014-11-30 23:00:12 -0800

    Convert studio listing page to angular template

--- www/js/app-service.js
+++ www/js/app-service.js
@@ -14,12 +14,49 @@
 	      	controller: 'StyleController',
 	      	controllerAs: 'styleCtrl'
 	      }).
+	      when('/studios', {
+	      	templateUrl: 'studios.html',
+	      	controller: 'StudioController',
+	      	controllerAs: 'studioCtrl'
+	      }).
 	      otherwise({
 	        redirectTo: '/moods'
 	      });
 	  }]);
 
-	var moods = [
+	var moods = [];
+	var styles = [];
+	var studios = [];
+
+	function getMoodByName(moodName) {
+		if(moodName == undefined) {
+			return undefined;
+		}
+
+		for(i = 0; i < moods.length; i++) {
+			if(moodName.toLowerCase() == moods[i].name.toLowerCase()) {
+				return moods[i];
+			}
+		}
+	};
+
+	app.controller('MoodController', function() {
+		this.moods = moods;
+	});
+
+	app.controller('StyleController', function($location) {
+		this.styles = styles;
+
+		var search = $location.search();
+		var moodParam = search.mood;
+		this.selectedMood = getMoodByName(moodParam);
+	});
+
+	app.controller('StudioController', function() {
+		this.studios = studios;
+	});
+
+	moods = [
 		{
 			"name": "Calm",
 			"chakra": "crown",
@@ -57,7 +94,7 @@
 		}
 	];
 
-	var styles = [
+	styles = [
 		{
 			"name": "Hatha",
 			"description": "Hatha is good."
@@ -68,28 +105,18 @@
 		}
 	];
 
-	function getMoodByName(moodName) {
-		if(moodName == undefined) {
-			return undefined;
+	studios = [
+		{
+			"name": "Just Yoga",
+			"address": "53 E Broadway",
+			"website": "http://justyoga.ca",
+			"photo": "img/studios/just_yoga_small.jpg"
+		},
+		{
+			"name": "Bikram Yoga Vancouver",
+			"address": "555 W 12th Ave",
+			"website": "http://www.bikramyogavancouver.com",
+			"photo": null
 		}
-
-		for(i = 0; i < moods.length; i++) {
-			if(moodName.toLowerCase() == moods[i].name.toLowerCase()) {
-				return moods[i];
-			}
-		}
-	};
-
-	app.controller('MoodController', function() {
-		this.moods = moods;
-	});
-
-	app.controller('StyleController', function($location) {
-		this.styles = styles;
-
-		var search = $location.search();
-		var moodParam = search.mood;
-		this.selectedMood = getMoodByName(moodParam);
-	});
-
+	];
 })();
