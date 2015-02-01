c5715bef55828d106947696ec240ff78da087875
Toby Sullivan <tobyjsullivan@gmail.com>
2015-02-01 13:35:28 -0800

    Download font locally to allow app runningoffline.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname+'/www')).listen(8080);
+console.log('Running on localhost:8080');
 
